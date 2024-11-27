c8703a15efb8f8f8649065df5fd1ab8bfdcfdace
maxfil333_BT <maxfil333@gmail.com>
2024-11-27 09:40:42 +0300

    init main (FastAPI)

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+# uvicorn src.main:app --reload
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все источники (можно ограничить)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы
+    allow_headers=["*"],  # Разрешить все заголовки
+)
+
+
+class Album(BaseModel):
+    name: str
+    artist: str
+
+
+albums = []
+
+
+@app.post("/albums/")
+def add_album(album: Album):
+    albums.append(album)
+    return {"message": "Альбом добавлен", "album": album}
+
+
+@app.get("/albums/")
+def show_albums():
+    return {"albums": [albums]}
+
+# GET: http://127.0.0.1:8000/albums
+
