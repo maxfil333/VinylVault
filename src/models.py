2c1cdddc7e7968e80297aba2f180fd074bb536cf
maxfil333_BT <maxfil333@gmail.com>
2025-01-23 10:06:20 +0300

    feat(core): models.py add Album and User models with ObjectId factory

--- src/models.py
+++ src/models.py
@@ -1,6 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from bson import ObjectId  # id для Mongo
 
 
 class Album(BaseModel):
+    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")  # alias для Mongo
     name: str
     artist: str
+    image: list
+
+
+class User(BaseModel):
+    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
+    username: str
+    password: str  # Хэшированный пароль
+
+
+if __name__ == '__main__':
+    user = User(username='user', password='pass')
+    print(user)
+
+    album = Album(name='album', artist='artist', image = [])
+    print(album)
