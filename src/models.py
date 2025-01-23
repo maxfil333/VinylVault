8779a28c4917922b3e7731f49eaa064c3ab10ce4
maxfil333_BT <maxfil333@gmail.com>
2025-01-23 13:33:46 +0300

    feat(core): model.py User add albums: list[Album]

--- src/models.py
+++ src/models.py
@@ -13,11 +13,17 @@ class User(BaseModel):
     id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
     username: str
     password: str  # Хэшированный пароль
+    albums: list[Album] = []
 
 
 if __name__ == '__main__':
     user = User(username='user', password='pass')
     print(user)
 
-    album = Album(name='album', artist='artist', image = [])
+    album = Album(name='album', artist='artist', image=[])
     print(album)
+
+    user.albums.append(album)
+    print(user)
+
+    User.model_validate(user.model_dump())  # валидация
