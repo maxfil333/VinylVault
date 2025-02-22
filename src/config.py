da2ceff09e938102bf1c8dc65164e92927ecdeb6
maxfil333 <maxfil333@gmail.com>
2025-02-23 00:20:44 +0300

    feat(core): RedirectResponse

--- src/config.py
+++ src/config.py
@@ -5,3 +5,7 @@ load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 URL = "http://ws.audioscrobbler.com/2.0/"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+WEBSITE_DIR = os.path.join(BASE_DIR, "..", "website")
+USERS_DIR = os.path.join(WEBSITE_DIR, "data", "users")
--- src/main.py
+++ src/main.py
@@ -2,21 +2,21 @@ import os
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pymongo.collection import Collection
 from pydantic import EmailStr
+
 
 from src.models import Album, User
 from src.album_info import album_search
 from src.database import vinyl_vault_users, add_user, is_in_collection
 from src.utils import load_html
+from src.pages import generate_user_page
+from src.config import WEBSITE_DIR, USERS_DIR
 
 app = FastAPI()
 # uvicorn src.main:app --reload
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-WEBSITE_DIR = os.path.join(BASE_DIR, "..", "website")
 
 app.add_middleware(
     CORSMiddleware,
@@ -55,7 +55,7 @@ def search_album(album_name: str):
 users_collection_dependency = Annotated[Collection, Depends(vinyl_vault_users)]
 
 
-@app.post("/register")
+@app.post("/register", response_class=HTMLResponse)
 async def register(users_collection: users_collection_dependency,
                    username: str = Form(...), password: str = Form(...), email: EmailStr = Form(...)):
     """ Обработчик регистрации. Принимает данные из HTML-формы и добавляет нового пользователя в базу данных. """
@@ -64,14 +64,17 @@ async def register(users_collection: users_collection_dependency,
         user = User(username=username, password=password, email=email)
         if is_in_collection(field='username', value=user.username, collection=users_collection):
             raise HTTPException(status_code=409, detail="User already exists")
-        add_user(users_collection, user)
+        new_user = add_user(users_collection, user)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка при регистрации пользователя: {e}")
 
-    return {
-        "message": "Пользователь успешно зарегистрирован",
-        "user_id": user.id
-    }
+    # Генерируем страницу для нового пользователя
+    generate_user_page(user_id=new_user.inserted_id, username=username)
+
+    # Перенаправляем пользователя на его страницу.
+    # Если страница генерируется в виде файла /users/{user_id}.html, то URL может выглядеть так:
+    return RedirectResponse(url=f"user/{new_user.inserted_id}.html", status_code=303)
+
 
 
 @app.post("/login")
@@ -98,6 +101,12 @@ async def welcome_page():
     return HTMLResponse(content=content)
 
 
+@app.get("/testuser", response_class=HTMLResponse)
+async def testuser():
+    content = load_html("user_page_example.html", WEBSITE_DIR)
+    return HTMLResponse(content=content)
+
+
 @app.get("/register", response_class=HTMLResponse)
 async def register_page():
     content = load_html("register.html", WEBSITE_DIR)
@@ -110,4 +119,8 @@ async def login_page():
     return HTMLResponse(content=content)
 
 
+# если кто-то запрашивает http://<your-domain>/static/somefile.png,
+# FastAPI ищет файл по пути WEBSITE_DIR/somefile.png на диске.
+# ! если прописывать путь без "/" в начале (например img src="static/...") - он будет не абсолютным, а относительным
 app.mount("/static", StaticFiles(directory=WEBSITE_DIR), name="static")
+app.mount("/user", StaticFiles(directory=USERS_DIR), name="user")
