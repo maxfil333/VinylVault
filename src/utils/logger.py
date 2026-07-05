62c33fcf54181e39c595a5a83c81160b46b425b6
maxfil333 <maxfil333@gmail.com>
2026-07-05 22:15:04 +0300

    refactor

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,7 +1,7 @@
 from loguru import logger as loguru_logger
 
 
-def setup_logger(log_file: str = "logs/app.log", level: str = "DEBUG"):
+def setup_logger(log_file: str = "__logs/app.log", level: str = "DEBUG"):
     loguru_logger.add(log_file,
                       format='{time} {level} {message}',
                       level=level,
